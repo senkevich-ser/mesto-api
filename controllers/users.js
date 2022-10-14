77eb3c4587d5ea9322ef5ef4dd53479ff8b8bb19
Sergey Senkevich <accc75-mai.ru@yandex.ru>
2022-10-14 06:01:19 +0400

    added apdate me & avatar controllers

--- controllers/users.js
+++ controllers/users.js
@@ -21,12 +21,22 @@ const createUser = (req, res) => {
     // данные не записались, вернём ошибку
     .catch((err) => res.status(500).send({ message: "Произошла ошибка", error: `${err}` }));
 };
-const updateAvatar = (req, res) => {
-  console.log(req.user._id)
-  const{avatar}=req.body
-  User.findByIdAndUpdate(req.user._id, { avatar: avatar })
-  .then(user => res.send({ data: user }))
-  .catch((err) => res.status(500).send({ message: "Произошла ошибка", error: `${err}` }));
+
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка", error: `${err}` }));
 };
 
-module.exports = { getUsers, createUser, getUser,updateAvatar };
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка", error: `${err}` }));
+};
+
+module.exports = {
+  getUsers, createUser, getUser, updateAvatar, updateProfile,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getUsers, createUser, getUser,updateAvatar } = require("../controllers/users");
+const {
+  getUsers, createUser, getUser, updateAvatar, updateProfile,
+} = require("../controllers/users");
 
 const routes = express.Router();
 
 routes.get("/users", getUsers);
 routes.get("/users/:id", getUser);
 routes.post("/users", express.json(), createUser);
+routes.patch("/users/me", express.json(), updateProfile);
 routes.patch("/users/me/avatar", express.json(), updateAvatar);
 
 module.exports = routes;
